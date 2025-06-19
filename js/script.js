553f51a8bc9374fb8aa3b2739592294b6f690f37
huthaifadl <huthaifasyr7@gmail.com>
2025-06-19 15:50:06 +0300

    start with js code

---
+++ js/script.js
@@ -0,0 +1,60 @@
+var siteNameInput = document.getElementById("site-name")
+var urlLinkInput = document.getElementById("url-link") 
+var bookMarksContainer;
+
+if(localStorage.getItem("bookMark") == null) {
+    bookMarksContainer = [];
+} else {
+    bookMarksContainer = JSON.parse( localStorage.getItem('bookMark'));
+    displayBookMark();
+}
+
+function addBookMark() {
+    var bookMark = {
+        siteName: siteNameInput.value,
+        siteURL: urlLinkInput.value
+    }
+    bookMarksContainer.push(bookMark);
+    // clear
+    clearInputs();
+    displayBookMark();
+    localStorage.setItem('bookMark' , JSON.stringify(bookMarksContainer));
+}
+
+function clearInputs() {
+    siteNameInput.value = "";
+    urlLinkInput.value = "";
+}
+
+function displayBookMark() {
+    var cardCartona = "";
+    var tableCartona = "";
+
+    // Card Loop
+    for(var i = 0; i < bookMarksContainer.length; i++) {
+        cardCartona += `
+        <div class="card" style="width: 18rem;">
+            <img src="./img/bookmarkimg.jpg" class="card-img-top" alt="google logo">
+                <div class="card-body text-center">
+                    <h5 class="card-title text-capitalize mb-4">${bookMarksContainer[i].siteName}</h5>
+                    <a href="${bookMarksContainer[i].siteURL}" target="_blank" class="btn btn-success text-capitalize"><i class="fa-solid fa-eye"></i> visit</a>
+                    <a href="#" class="btn btn-danger text-capitalize"><i class="fa-solid fa-trash"></i> delete</a>
+                </div>
+        </div>
+        `;
+    }
+
+    // Table Loop
+    for(var i = 0; i < bookMarksContainer.length; i++) {
+        tableCartona += `
+        <tr>
+            <td>${i + 1}</td>
+            <td>${bookMarksContainer[i].siteName}</td>
+            <td><button onclick="window.open('${bookMarksContainer[i].siteURL}', '_blank')" type="button" class="btn btn-success"><i class="fa-solid fa-eye"></i> Visit</button></td>
+            <td><button type="button" class="btn btn-danger"><i class="fa-solid fa-trash"></i> Delete</button></td>
+        </tr>
+        `;
+    }
+    document.getElementById('body-table').innerHTML = tableCartona;
+    document.getElementById('cardsData').innerHTML = cardCartona;
+}
